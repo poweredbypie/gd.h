cf17241d8eb9865c67a6ceb1e07306eb56366c73
PoweredByPie <67135060+poweredbypie@users.noreply.github.com>
2021-03-10 10:58:38 -0800

    didnt actually fully define pad

--- include/gdMacros.h
+++ include/gdMacros.h
@@ -1,6 +1,12 @@
 #ifndef __GDMACROS_H__
 #define __GDMACROS_H__
 
+/** __STR_CAT__(str)
+* Concatenate 2 tokens. Don't use this.
+*/
+#define __STR_CAT___(str1, str2) str1##str2
+#define __STR_CAT__(str1, str2) __STR_CAT___(str1, str2)
+
 /** PAD
 * Add padding to a class / struct. For shifting classes /
 * structs to be aligned, if too lazy to fully reverse.
@@ -9,4 +15,4 @@
 */
 #define PAD(size) char __STR_CAT__(pad, __LINE__)[size] = {};
 
-#endif
+#endif 

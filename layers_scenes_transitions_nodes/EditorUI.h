0337c6c439f0c400bd5e9b294404f76d777c29a1
HJfod <60038575+HJfod@users.noreply.github.com>
2021-04-29 11:35:46 +0300

    fix

--- layers_scenes_transitions_nodes/EditorUI.h
+++ layers_scenes_transitions_nodes/EditorUI.h
@@ -6,7 +6,16 @@
 namespace gd {
 
 class EditorUI : public cocos2d::CCLayer {
-    // todo
+    public:
+        cocos2d::CCArray* pasteObjects(std::string const& _str) {
+            return reinterpret_cast<cocos2d::CCArray*(__thiscall*)(
+                EditorUI*, std::string
+            )>(
+                base + 0x88240
+            )(
+                this, _str
+            );
+        }
 };
 
 }

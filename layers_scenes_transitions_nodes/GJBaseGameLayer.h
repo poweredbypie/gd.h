76f7f080246890445e9ecce768846fe292ed9ae2
matcool <26722564+matcool@users.noreply.github.com>
2021-05-06 15:40:24 -0300

    the messiest playlayer class ever

--- layers_scenes_transitions_nodes/GJBaseGameLayer.h
+++ layers_scenes_transitions_nodes/GJBaseGameLayer.h
@@ -4,8 +4,19 @@
 #include <gd.h>
 
 namespace gd {
+
+	class PlayerObject;
+	class LevelSettingsObject;
+	
 	class GJBaseGameLayer : public cocos2d::CCLayer {
-		//idk
+	public:
+		PAD(264);
+		PlayerObject* player1; //0x0224
+		PlayerObject* player2; //0x0228
+		LevelSettingsObject* levelSettings; //0x022C
+		PAD(128);
+		bool unk_2B0;
+		PAD(27);
 	};
 }
 

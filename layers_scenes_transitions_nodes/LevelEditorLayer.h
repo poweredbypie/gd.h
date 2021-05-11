2f4d7b0fde011b87a68952d94fcb119f5bd7e8a6
HJfod <60038575+HJfod@users.noreply.github.com>
2021-05-11 22:04:34 +0300

    LevelEditorLayer::removeObject and getAllObjects

--- layers_scenes_transitions_nodes/LevelEditorLayer.h
+++ layers_scenes_transitions_nodes/LevelEditorLayer.h
@@ -10,12 +10,26 @@ class EditorUI;
 
 class LevelEditorLayer : public GJBaseGameLayer {
     protected:
-        PAD(0x264)
+        PAD(0x118)
+        cocos2d::CCArray* m_pObjects;
+        PAD(0X148)
         EditorUI* m_pEditorUI;
 
     public:
         EditorUI* getEditorUI() { return this->m_pEditorUI; }
-};
+
+        cocos2d::CCArray* getAllObjects() { return this->m_pObjects; }
+
+        void removeObject(GameObject * obj, bool idk) {
+            reinterpret_cast<void(__thiscall*)(
+                LevelEditorLayer *,GameObject *,bool
+            )>(
+                base + 0x161cb0
+            )(
+                this, obj, idk
+            );
+        } 
+    };
 
 }
 

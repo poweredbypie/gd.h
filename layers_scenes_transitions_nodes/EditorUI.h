2cb70c6cc43bb51ac4b5d3d72e664256308b49b9
HJfod <60038575+HJfod@users.noreply.github.com>
2021-05-19 20:47:07 +0300

    getAllObjects() for editorui

--- layers_scenes_transitions_nodes/EditorUI.h
+++ layers_scenes_transitions_nodes/EditorUI.h
@@ -6,6 +6,12 @@
 namespace gd {
 
 class EditorUI : public cocos2d::CCLayer {
+    protected:
+        PAD(0x118)
+        cocos2d::CCArray* m_pObjects;
+        PAD(0X148)
+        EditorUI* m_pEditorUI;
+        
     public:
         cocos2d::CCArray* pasteObjects(std::string const& _str) {
             auto ret = reinterpret_cast<cocos2d::CCArray*(__thiscall*)(
@@ -20,11 +26,24 @@ class EditorUI : public cocos2d::CCLayer {
 
             return ret;
         }
+
         cocos2d::CCArray* getSelectedObjects() {
             return reinterpret_cast<cocos2d::CCArray*(__thiscall*)(EditorUI*)>(
                 base + 0x86900
             )(this);
         }
+
+        cocos2d::CCArray* getAllObjects() { return this->m_pObjects; }
+
+        void removeObject(GameObject * obj, bool idk) {
+            reinterpret_cast<void(__thiscall*)(
+                LevelEditorLayer*, GameObject*, bool
+            )>(
+                base + 0x161cb0
+            )(
+                this, obj, idk
+            );
+        } 
 };
 
 }

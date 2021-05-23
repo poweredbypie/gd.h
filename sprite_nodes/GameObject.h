e55196e611c8ed8cc9a602b4cb6759268bc9408c
matcool <26722564+matcool@users.noreply.github.com>
2021-05-23 18:57:44 -0300

    clean up gameobject

--- sprite_nodes/GameObject.h
+++ sprite_nodes/GameObject.h
@@ -7,10 +7,46 @@ namespace gd {
 
 	class CCSpritePlus;
 
+	// jesus fucking christ (painfully written by @hjfod)
 	enum GameObjectType {
-		Solid = 0,
-		Hazard = 2
-		// too lazy to add the rest, theyre all here https://gist.github.com/absoIute/c8fa23c9b2cb39252755465345bc6e35#file-blocks-h-L1615
+		kGameObjectTypeSolid = 0,
+		kGameObjectTypeHazard = 2,
+		kGameObjectTypeInverseGravityPortal = 3,
+		kGameObjectTypeNormalGravityPortal = 4,
+		kGameObjectTypeShipPortal = 5,
+		kGameObjectTypeCubePortal = 6,
+		kGameObjectTypeDecoration = 7,
+		kGameObjectTypeYellowJumpPad = 8,
+		kGameObjectTypePinkJumpPad = 9,
+		kGameObjectTypeGravityPad = 10,
+		kGameObjectTypeYellowJumpRing = 11,
+		kGameObjectTypePinkJumpRing = 12,
+		kGameObjectTypeGravityRing = 13,
+		kGameObjectTypeInverseMirrorPortal = 14,
+		kGameObjectTypeNormalMirrorPortal = 15,
+		kGameObjectTypeBallPortal = 16,
+		kGameObjectTypeRegularSizePortal = 17,
+		kGameObjectTypeMiniSizePortal = 18,
+		kGameObjectTypeUfoPortal = 19,
+		kGameObjectTypeModifier = 20,
+		kGameObjectTypeSecretCoin = 22,
+		kGameObjectTypeDualPortal = 23,
+		kGameObjectTypeSoloPortal = 24,
+		kGameObjectTypeSlope = 25,
+		kGameObjectTypeWavePortal = 26,
+		kGameObjectTypeRobotPortal = 27,
+		kGameObjectTypeTeleportPortal = 28,
+		kGameObjectTypeCollectible = 30,
+		kGameObjectTypeUserCoin = 31,
+		kGameObjectTypeDropRing = 32,
+		kGameObjectTypeSpiderPortal = 33,
+		kGameObjectTypeRedJumpPad = 34,
+		kGameObjectTypeRedJumpRing = 35,
+		kGameObjectTypeCustomRing = 36,
+		kGameObjectTypeDashRing = 37,
+		kGameObjectTypeGravityDashRing = 38,
+		kGameObjectTypeCollisionObject = 39,
+		kGameObjectTypeSpecial = 40,
 	};
 
 	class GJSpriteColor;
@@ -21,92 +57,104 @@ namespace gd {
 	class GameObject : public CCSpritePlus {
 	public:
 		PAD(44);
-		bool unk_21C;
-		bool unk_21D;
-		bool unk_21E;
+		bool m_unk21C;
+		bool m_unk21D;
+		bool m_unk21E;
 		PAD(13);
-		cocos2d::CCPoint realPositionOffset; //0x022C
+		cocos2d::CCPoint m_startPosOffset; //0x22C
 		PAD(4);
-		bool unk_238;
-		bool isFlippedX; //0x0239
-		bool isFlippedY; //0x023A
-		PAD(9);
-		bool isOriented; //0x0244 // what
-		PAD(35);
-		float objectRectWidth; //0x0268
-		float objectRectHeight; //0x026C
-		bool unk_270;
-		bool unk_271;
-		bool unk_272;
+		bool m_unk238;
+		bool m_isFlippedX; //0x239
+		bool m_isFlippedY; //0x23A
 		PAD(1);
-		cocos2d::CCParticleSystemQuad* unk_274;
-		std::string effectPlistName; //0x0278
+		cocos2d::CCPoint m_boxOffset; //0x23C
+		bool m_isOriented; //0x244 idek what this is
+		PAD(3);
+		cocos2d::CCPoint m_unk248; // related to box offset
+		PAD(4);
+		bool m_unk254;
+		PAD(11);
+		cocos2d::CCAction* m_action; //0x260
+		PAD(4);
+		cocos2d::CCSize m_objectSize; //0x268
+		bool m_unk270;
+		bool m_unk271;
+		bool m_unk272;
+		PAD(1);
+		cocos2d::CCParticleSystemQuad* m_particleSystem;
+		std::string m_effectPlistName; //0x278
 		PAD(32);
-		bool unk_2B0;
-		PAD(23);
-		bool unk_2C8;
-		bool unk_2C9;
-		bool hasBeenActivated; //0x02CA
-		PAD(25);
-		cocos2d::CCSprite* unk_2E4;
-		PAD(8);
-		bool isRotatedSide; //0x02F0 // what
-		PAD(3);
-		float unk_2F4;
-		float unk_2F8;
-		unsigned int uniqueID; //0x02FC
-		GameObjectType objectType; //0x0300
-		int section; //0x0304
-		PAD(4);
-		cocos2d::CCPoint realPosition; //0x030C
-		std::string textureName; //0x0314
-		bool unk_32C;
-		bool unk_32D;
-		PAD(14);
-		float unk_33C;
-		float unk_340;
-		PAD(16);
-		bool hasGlowDisabled; //0x0354
+		bool m_unk2B0; // has custom scale or moved idfk
 		PAD(7);
-		float scale; //0x035C
-		int objectID; //0x0360
-		PAD(4);
-		bool unk_368;
-		bool unk_369;
-		bool unk_36A;
-		bool dontEnter; //0x036B
-		bool dontFade; //0x036C
-		PAD(35);
-		float unk_390;
-		PAD(20);
-		GJSpriteColor* baseColor; //0x03A8
-		GJSpriteColor* detailColor; //0x03AC
-		PAD(8);
-		int zLayer; //0x03B8
-		int zOrder; //0x03BC
+		cocos2d::CCRect m_objectRect2; //0x2B8
+		bool m_isObjectRectDirty; //0x2C8
+		bool m_isOrientedRectDirty; //0x2C9
+		bool m_hasBeenActivated; //0x2CA
+		bool m_hasBeenActivatedP2; //0x2CB
 		PAD(24);
-		bool unk_3D8;
-		bool unk_3D9;
-		bool m_bIsSelected; //0x03DA
-		PAD(1);
-		int globalClickCounter; //0x03DC // this is rly weird
-		PAD(12);
-		float unk_3EC;
-		bool groupParent; //0x03F0
+		cocos2d::CCSprite* m_unkSprite; //0x2E4
+		PAD(8);
+		bool m_isRotatedSide; //0x2F0 for 90 and 270 degrees rotations
 		PAD(3);
-		short** groups; //0x03F4
-		short groupCount; //0x03F8
-		PAD(18);
-		int editorLayer; //0x040C
-		int editorLayer2; //0x0410
+		float m_unk2F4;
+		float m_unk2F8;
+		int m_uniqueID; //0x2FC
+		GameObjectType m_objectType; //0x300
+		int m_section; //0x304
+		PAD(4);
+		cocos2d::CCPoint m_startPosition; //0x30C
+		std::string m_textureName; //0x314
+		bool m_unk32C;
+		bool m_unk32D;
+		PAD(14);
+		float m_unk33C;
+		float m_unk340;
 		PAD(16);
-		cocos2d::CCPoint startPos; //0x0424 // maybe
-		PAD(28);
-		bool highDetail; //0x0448
+		bool m_isGlowDisabled; //0x354
+		PAD(7);
+		float m_scale; //0x35C
+		int m_objectID; //0x360
+		PAD(4);
+		bool m_unk368;
+		bool m_unk369;
+		bool m_unk36A;
+		bool m_isDontEnter; //0x36B
+		bool m_isDontFade; //0x36C
+		PAD(31);
+		bool m_unk38C;
+		bool m_unk38D;
+		bool m_unk38E;
+		PAD(1);
+		float m_unk390;
+		PAD(20);
+		GJSpriteColor* m_baseColor; //0x3A8
+		GJSpriteColor* m_detailColor; //0x3AC
+		PAD(8);
+		int m_zLayer; //0x3B8
+		int m_zOrder; //0x3BC
+		std::string m_unk3C0;
+		bool m_unk3D8;
+		bool m_unk3D9;
+		bool m_isSelected; //0x3DA
+		PAD(1);
+		int m_globalClickCounter; //0x3DC i have no idea what this is for
+		PAD(12);
+		float m_unk3EC;
+		bool m_isGroupParent; //0x3F0
 		PAD(3);
-		ColorActionSprite* unk_44C;
-		ColorActionSprite* unk_450;
-		GJEffectManager* unk_454;
+		short* m_groups; //0x3F4
+		short m_groupCount; //0x3F8
+		PAD(18);
+		int m_editorLayer; //0x40C
+		int m_editorLayer2; //0x410
+		PAD(16);
+		cocos2d::CCPoint m_startPos; //0x424 uh oh
+		PAD(28);
+		bool m_isHighDetail; //0x448
+		PAD(3);
+		ColorActionSprite* m_colorActionSprite1; //0x44C
+		ColorActionSprite* m_colorActionSprite2; //0x450
+		GJEffectManager* m_effectManager; //0x454
 		PAD(16);
 
 		//CCNode vtable
@@ -172,7 +220,7 @@ namespace gd {
 		}
 
 		void deselectObject() {
-			this->m_bIsSelected = false;
+			this->m_isSelected = false;
 
 			return reinterpret_cast<void(__fastcall*)(GameObject*)>(
 				base + 0xeee50

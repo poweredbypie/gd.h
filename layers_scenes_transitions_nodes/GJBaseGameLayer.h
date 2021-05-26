fad8cf461b27a1b352ad987c39f4faab4046953d
matcool <26722564+matcool@users.noreply.github.com>
2021-05-25 22:43:41 -0300

    better names

--- layers_scenes_transitions_nodes/GJBaseGameLayer.h
+++ layers_scenes_transitions_nodes/GJBaseGameLayer.h
@@ -4,18 +4,17 @@
 #include <gd.h>
 
 namespace gd {
-
 	class PlayerObject;
 	class LevelSettingsObject;
 	
 	class GJBaseGameLayer : public cocos2d::CCLayer {
 	public:
 		PAD(264);
-		PlayerObject* player1; //0x0224
-		PlayerObject* player2; //0x0228
-		LevelSettingsObject* levelSettings; //0x022C
+		PlayerObject* m_player1; //0x0224
+		PlayerObject* m_player2; //0x0228
+		LevelSettingsObject* m_levelSettings; //0x022C
 		PAD(128);
-		bool unk_2B0;
+		bool m_unk2B0;
 		PAD(27);
 	};
 }

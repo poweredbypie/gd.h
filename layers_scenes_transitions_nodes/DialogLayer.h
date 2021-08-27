3c002758615a931ee66f161cde40a29017e30488
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-08-27 18:39:55 +0100

    Update DialogLayer.h

--- layers_scenes_transitions_nodes/DialogLayer.h
+++ layers_scenes_transitions_nodes/DialogLayer.h
@@ -5,7 +5,20 @@
 
 namespace gd {
     class DialogLayer : public cocos2d::CCLayerColor {
-        // TODO
+        public:
+            char pad1[0x8];
+            cocos2d::CCLayer *m_pLayer;
+            cocos2d::CCLabelBMFont *m_pTitle;
+            TextArea *m_pDialog;
+            cocos2d::CCSprite *m_pIcon;
+            cocos2d::CCArray *m_pDialogObjects;
+            char PAD2[0X4];
+            cocos2d::CCSprite *m_pChatBtn;
+            bool m_bUnk;
+            char m_bCanSkip;
+            char PAD3[0x2];
+            DWORD m_pUnk;
+            char PAD4[0x8];
     };
 }
 

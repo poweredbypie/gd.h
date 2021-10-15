80aa75caffa1ea80ada0ec09062b4faa39901292
HJfod <60038575+HJfod@users.noreply.github.com>
2021-10-15 22:04:48 +0300

    MoreOptionsLayer create

--- layers_scenes_transitions_nodes/MoreOptionsLayer.h
+++ layers_scenes_transitions_nodes/MoreOptionsLayer.h
@@ -4,11 +4,17 @@
 #include <gd.h>
 
 namespace gd {
-    class MoreOptionsLayer : public cocos2d::CCLayer {
-    public:
-        void addToggle(const char* name, const char* key, const char* info) {
-            reinterpret_cast<void*(__thiscall*)(MoreOptionsLayer*, const char*, const char*, const char*)>(base + 0x1DF6B0)(this, name, key, info);
-        }
+    class MoreOptionsLayer : public FLAlertLayer {
+        public:
+            static MoreOptionsLayer* create() {
+                return reinterpret_cast<MoreOptionsLayer*(__fastcall*)()>(
+                    base + 0x1de850
+                )();
+            }
+
+            void addToggle(const char* name, const char* key, const char* info) {
+                reinterpret_cast<void*(__thiscall*)(MoreOptionsLayer*, const char*, const char*, const char*)>(base + 0x1DF6B0)(this, name, key, info);
+            }
     };
 }
 
